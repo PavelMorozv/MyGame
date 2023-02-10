4101df2fcc75a076e3dc9ebd1199f20c9bdf4d38
PavelMorozovTK <PavelMorozovTK@yandex.ru>
2023-02-10 08:45:00 +0700

    Добавление классов сервреа и клиента

---
+++ MyNetworkLibrary/Client.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.Sockets;
+using System.Text.Json;
+
+namespace MyNetworkLibrary
+{
+    public class Client
+    {
+        TcpClient tcpClient;
+        public Guid Id { get; set; } = Guid.Empty;
+        public string Name { get; set; } = "Unknown";
+        public int Status { get; set; }
+
+        public Guid RoomID { get; set; } = Guid.Empty;
+        public bool IsAuth { get; set; } = false;
+
+        public bool IsCnnect { get; private set; } = false;
+        public bool IsAvailable { get { return tcpClient.Available >= 4; } }
+
+        public Client(string hostIP, int port)
+        {
+            tcpClient = new TcpClient(hostIP, port);
+        }
+
+        public Client(TcpClient tcpClient)
+        {
+            this.tcpClient = tcpClient;
+        }
+
+        public string Read()
+        {
+            string result = string.Empty;
+            byte[] dataBytes = new byte[sizeof(int)];
+            if (IsAvailable)
+            {
+                NetworkStream stream = tcpClient.GetStream();
+
+                stream.Read(dataBytes, 0, dataBytes.Length);
+                int dataSize = BitConverter.ToInt32(dataBytes);
+
+
+                stream.Read(dataBytes, 0, dataSize);
+                result = JsonSerializer.Deserialize<string>(dataBytes);
+                
+            }
+
+            return result;
+        }
+
+        public void Write(string data)
+        {
+            var dataBytes = JsonSerializer.SerializeToUtf8Bytes(data);
+            var dataSize = BitConverter.GetBytes(dataBytes.Length);
+
+            List<byte> bytesRecive = new List<byte>();
+            bytesRecive.AddRange(dataSize);
+            bytesRecive.AddRange(dataBytes);
+
+            tcpClient.GetStream().Write(bytesRecive.ToArray(), 0, bytesRecive.Count);
+        }
+    }
+}
---
+++ MyNetworkLibrary/Server.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Sockets;
+
+namespace MyNetworkLibrary
+{
+    public class Server
+    {
+
+        private TcpListener TcpListener { get; set; }
+        private Queue<TcpClient> clients = new Queue<TcpClient>();
+
+        public TcpClient GetClients { get { Console.WriteLine("Клиентов в очереди на подключение: " + IsNewConnected);  return clients.Dequeue(); } }
+        public int IsNewConnected { get { return clients.Count; } }
+        public bool isRun { get; private set; }
+
+
+
+        public void Start()
+        {
+            isRun = true;
+
+            TcpListener.Start();
+            Task.Run(() => {
+                while (isRun)
+                {
+                    Process();
+                    Thread.Sleep(100);
+                }
+            });
+        }
+        public void Stop()
+        {
+            TcpListener.Stop();
+        }
+
+
+
+        public void Process()
+        {
+            ConnectingСlients();
+            Console.WriteLine("Await client connection...");
+        }
+        
+        public void ConnectingСlients()
+        {
+            if (!TcpListener.Pending()) return;
+            Console.WriteLine("New connection...");
+            clients.Enqueue(TcpListener.AcceptTcpClient());
+        }
+    }
+}
