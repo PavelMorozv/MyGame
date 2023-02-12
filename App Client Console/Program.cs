fdc51a2f23804167da6059ed0ee54f2867e88a4e
PavelMorozovTK <PavelMorozovTK@yandex.ru>
2023-02-12 22:01:09 +0700

    Добавление к прошлой фиксации

---
+++ App Client Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyNetworkLibrary.Classes;
+
+namespace App_Client_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client("127.0.0.1", 8888);
+
+            do
+            {
+                if (client.IsAvailable)
+                {
+                    Console.WriteLine("Server recive" + client.Read());
+                }
+                Thread.Sleep(100);
+            } while (true);
+        }
+    }
+}
---
+++ App Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyNetworkLibrary.Classes;
+
+namespace App_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementServer server = new ManagementServer(new ConnectionServer("127.0.0.1", 8888));
+            server.Start();
+
+
+            string com;
+
+            do
+            {
+                com = Console.ReadLine();
+
+                if (com == "stop")
+                {
+                    server.Stop();
+                }
+
+            } while (server.isRun);
+
+        }
+    }
+}
